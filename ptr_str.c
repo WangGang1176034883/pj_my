76862610242ff0bc513a9a886b232ade666e8946
wg <1176034883@qq.com>
2022-10-24 17:30:41 +0800

    v2022.10.24.17_twoArray_define.c二维数组和指针

--- ptr_str.c
+++ ptr_str.c
@@ -1,109 +1,109 @@
 #include <stdio.h>
 #include <string.h>
 void str(){
-    char string[] = "I Love China!";
-    printf("%s\n", string);
-    printf("%s\n", string+7);
-    char* p;
-    p = string;
-    p += 7;
-    while(*p){
-        putchar(p[0]);
-        p++;
-    }
+	char string[] = "I Love China!";
+	printf("%s\n", string);
+	printf("%s\n", string+7);
+	char* p;
+	p = string;
+	p += 7;
+	while(*p){
+		putchar(p[0]);
+		p++;
+	}
 }
 void str_space(){
-    char s[8], *pi, *pj;
-    int i, j, n;
-    fgets(s, 8, stdin);
-    n=strlen(s);
-    //pi 和 pj 分别指向字符 串 首 末
-    pi=s;
-    pj=s+n-1;
-    //跳 过输入时的前 导 空 格 
-    while ( *pi== ' ') pi++;
-    //跳 过输入时的 尾 随空 格 
-    while ( *pj== ' ') pj--;
-    while ((pi<pj)&&( *pi== *pj)) {
-        pi++;
-        pj--;
-    }
-    if ( pi<pj) {
-        printf("NO\n");
-    }
-    else {
-        printf("YES\n");
-    }
+	char s[8], *pi, *pj;
+	int i, j, n;
+	fgets(s, 8, stdin);
+	n=strlen(s);
+	//pi 和 pj 分别指向字符串首末
+	pi=s;
+	pj=s+n-1;
+	//跳 过输入时的前导空格 
+	while ( *pi== ' ') pi++;
+	//跳 过输入时的尾随空格
+	while ( *pj== ' ') pj--;
+	while ((pi<pj)&&( *pi== *pj)) {
+		pi++;
+		pj--;
+	}
+	if ( pi<pj) {
+		printf("NO\n");
+	}
+	else {
+		printf("YES\n");
+	}
 }
 void argum_ptr_str(){
-    char a[]="I am a teacher.";
-    char b[]="You are a student.";
-    printf("string_a = %s\nstring_b = %s\n\n",a,b);
-    char *p, *q;
-    for(p=a, q=b;*p != '\0';p++,q++) {
-        putchar(*p);
-        *p = *q;
-        putchar(*p);
-    }
-    printf("\n\n");
-    printf("string_a = %s\nstring_b = %s\n",a,b);
-    printf("\narray_a = %s\narray_b = %s\n\n",a,b);
-    int i = 0;
-    while(a[i] != '\0'){
-        putchar(a[i]);
-        a[i] = b[i];
-        putchar(a[i]);
-        i++;
-    }
-    printf("\n\n");
-    printf("array_a = %s\narray_b = %s\n",a,b);
+	char a[]="I am a teacher.";
+	char b[]="You are a student.";
+	printf("string_a = %s\nstring_b = %s\n\n",a,b);
+	char *p, *q;
+	for(p=a, q=b;*p != '\0';p++,q++) {
+		putchar(*p);
+		*p = *q;
+		putchar(*p);
+	}
+	printf("\n\n");
+	printf("string_a = %s\nstring_b = %s\n",a,b);
+	printf("\narray_a = %s\narray_b = %s\n\n",a,b);
+	int i = 0;
+	while(a[i] != '\0'){
+		putchar(a[i]);
+		a[i] = b[i];
+		putchar(a[i]);
+		i++;
+	}
+	printf("\n\n");
+	printf("array_a = %s\narray_b = %s\n",a,b);
 }
 void rev_str(){
-    char s[50] ={
-    '\0'}
-    ;
-    char* p, *q;
-    char temp;
-    printf("输入字符串:");
-    fgets(s,50,stdin);
-    for(int i = 0;i < 25;i++) {
-        temp = s[i];
-        s[i] = s[50-1-i];
-        s[50-1-i] = temp;
-    }
-    for(int i = 0;i < 50;i++) {
-        printf("%3u ", s[i]);
-    }
+	char s[50] ={
+		'\0'}
+	;
+	char* p, *q;
+	char temp;
+	printf("输入字符串:");
+	fgets(s,50,stdin);
+	for(int i = 0;i < 25;i++) {
+		temp = s[i];
+		s[i] = s[50-1-i];
+		s[50-1-i] = temp;
+	}
+	for(int i = 0;i < 50;i++) {
+		printf("%3u ", s[i]);
+	}
 }
 void stringcat(){
-    char a[81] = "hello";
-    char b[81] = "hunan university";
-    char *p,*q;
-    /*
-    for(p=a,q=b;*q!='\0';p++) {
-        if(*p=='\0')
-        {
-            *p=*q;
-            q++;
-        }
-    }
-    
-    */
-    p=a,q=b;
-    while(*p!='\0') p++;
-    while(*q!='\0'){
-        *p=*q;
-        p++;
-        q++;
-    }
-    //*p=*q;
-   printf("%s", a);
+	char a[81] = "hello";
+	char b[81] = "hunan university";
+	char *p,*q;
+	/*
+	   for(p=a,q=b;*q!='\0';p++) {
+	   if(*p=='\0')
+	   {
+	 *p=*q;
+	 q++;
+	 }
+	 }
+
+*/
+	p=a,q=b;
+	while(*p!='\0') p++;
+	while(*q!='\0'){
+		*p=*q;
+		p++;
+		q++;
+	}
+	//*p=*q;
+	printf("%s", a);
 }
 int main(int argc,char** argv) {
-    //str();
-    //str_space();
-    //argum_ptr_str();
-    //rev_str();
-    stringcat();
+	//str();
+	//str_space();
+	//argum_ptr_str();
+	//rev_str();
+	stringcat();
 }
 
--- test.c
+++ test.c
@@ -1,242 +1,247 @@
 #include <stdio.h>
-void mnstar(int m, int n){
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j < n;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+mstar(int m){
+	int for = space
+		int(int i = 0;i <m;i++) {
+			space--;
+			for(int j = 0;j <2*i+1;j++) {
+				vepprintf("*");
+			}
+			printf("\n");
+		}
 }
-m; mstar(int m){
-    int for = space
-    int(int i = 0;i <m;i++) {
-        YAfor(int n= 1;n < spacedf;n++) {G
-G
-		I
-            printf(" ");
-        }
-        space--;
-        for(int j = 0;j <2*i+1;j++) {
-           vepprintf("*");
-        }
-        printf("\n");
-    }
+mstar(int m){
+	int for = space
+		int(int i = 0;i <m;i++) {
+			space--;
+			for(int j = 0;j <2*i+1;j++) {
+				vepprintf("*");
+			}
+			printf("\n");
+		}
+}
+void mnstar(int m, int n){
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j < n;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void rectstar(int m){
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j <= i;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    int space = 1;
-    for(int i = m;i >1;i--) {
-        for(int k = 0;k < space;k++) {
-            printf(" ");
-        }
-        space++;
-        for(int j = 0;j < i-1;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j <= i;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	int space = 1;
+	for(int i = m;i >1;i--) {
+		for(int k = 0;k < space;k++) {
+			printf(" ");
+		}
+		space++;
+		for(int j = 0;j < i-1;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void doubstar(int m){
-    int space = m;
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j < 2*i+1;j++) {
-            printf("*");
-        }
-        for(int k = 0;k < 5*space-5;k++) {
-            printf(" ");
-        }
-        space--;
-        for(int j = 0;j < 2*i+1;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	int space = m;
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j < 2*i+1;j++) {
+			printf("*");
+		}
+		for(int k = 0;k < 5*space-5;k++) {
+			printf(" ");
+		}
+		space--;
+		for(int j = 0;j < 2*i+1;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void udstar(int m){
-    int space = m-1;
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k < space;k++) {
-            printf(" ");
-        }
-        space--;
-        for(int j = 0;j < 2*i+1;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    int star = 2*(m-1)-1;
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k <= i;k++) {
-            printf(" ");
-        }
-        for(int j = 0;j < star;j++) {
-            printf("*");
-        }
-        star -=2;
-        printf("\n");
-    }
+	int space = m-1;
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k < space;k++) {
+			printf(" ");
+		}
+		space--;
+		for(int j = 0;j < 2*i+1;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	int star = 2*(m-1)-1;
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k <= i;k++) {
+			printf(" ");
+		}
+		for(int j = 0;j < star;j++) {
+			printf("*");
+		}
+		star -=2;
+		printf("\n");
+	}
 }
 void leftstar(int m){
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j < 2*i+1;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    int star = 2*(m-1)-1;
-    for(int i = 0;i < m-1;i++) {
-        for(int j = 0;j < star;j++) {
-            printf("*");
-        }
-        printf("\n");
-        star -=2;
-    }
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j < 2*i+1;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	int star = 2*(m-1)-1;
+	for(int i = 0;i < m-1;i++) {
+		for(int j = 0;j < star;j++) {
+			printf("*");
+		}
+		printf("\n");
+		star -=2;
+	}
 }
 void rightstar(int m){
-    int space =2*(m-1);
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j < space;j++) {
-            printf(" ");
-        }
-        space -=2;
-        for(int k = 0;k < 2*i+1;k++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    int star =2*(m-1)-1;
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k < 2*i+2;k++) {
-            printf(" ");
-        }
-        for(int j = 0;j <star;j++) {
-            printf("*");
-        }
-        printf("\n");
-        star -=2;
-    }
+	int space =2*(m-1);
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j < space;j++) {
+			printf(" ");
+		}
+		space -=2;
+		for(int k = 0;k < 2*i+1;k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	int star =2*(m-1)-1;
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k < 2*i+2;k++) {
+			printf(" ");
+		}
+		for(int j = 0;j <star;j++) {
+			printf("*");
+		}
+		printf("\n");
+		star -=2;
+	}
 }
 void dustar(int m){
-    int star = 2*m-1;
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k < i;k++) {
-            printf(" ");
-        }
-        for(int j = 0;j < star;j++) {
-            printf("*");
-        }
-        star -=2;
-        printf("\n");
-    }
-    int space = m-1;
-    for(int i = 0;i < m;i++) {
-        for(int j = 0;j < space;j++) {
-            printf(" ");
-        }
-        space--;
-        for(int k = 0;k < 2*i+1;k++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	int star = 2*m-1;
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k < i;k++) {
+			printf(" ");
+		}
+		for(int j = 0;j < star;j++) {
+			printf("*");
+		}
+		star -=2;
+		printf("\n");
+	}
+	int space = m-1;
+	for(int i = 0;i < m;i++) {
+		for(int j = 0;j < space;j++) {
+			printf(" ");
+		}
+		space--;
+		for(int k = 0;k < 2*i+1;k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void greatstar(int m){
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k < i;k++) {
-            printf(" ");
-        }
-        for(int j = 0;j < 7;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    int space = m-2;
-    for(int i = 0;i < m-1;i++) {
-        for(int k = 0;k < space;k++) {
-            printf(" ");
-        }
-        space--;
-        for(int j = 0;j < 7;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k < i;k++) {
+			printf(" ");
+		}
+		for(int j = 0;j < 7;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	int space = m-2;
+	for(int i = 0;i < m-1;i++) {
+		for(int k = 0;k < space;k++) {
+			printf(" ");
+		}
+		space--;
+		for(int j = 0;j < 7;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void lessstar(int m){
-    int space = m-1;
-    for(int i = 0;i < m;i++) {
-        for(int k = 0;k < space;k++) {
-            printf(" ");
-        }
-        space--;
-        for(int j = 0;j < 7;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    for(int i = 0;i < m-1;i++) {
-        for(int k = 0;k <= i;k++) {
-            printf(" ");
-        }
-        for(int j = 0;j < 7;j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	int space = m-1;
+	for(int i = 0;i < m;i++) {
+		for(int k = 0;k < space;k++) {
+			printf(" ");
+		}
+		space--;
+		for(int j = 0;j < 7;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(int i = 0;i < m-1;i++) {
+		for(int k = 0;k <= i;k++) {
+			printf(" ");
+		}
+		for(int j = 0;j < 7;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 }
 void upstar(int m){
-    int mstar = m/2;
-    int mspace = m-mstar;
-    int space = mspace;
-    for(int i = 0;i < mspace;i++) {
-        for(int k = 0;k < space;k++) {
-            printf("   ");
-        }
-        space--;
-        for(int j = 0;j < 2*i+1;j++) {
-            printf("*  ");
-        }
-        printf("\n");
-    }
-    for(int i = 0;i < mstar;i++) {
-        for(int j = 0;j < m+2;j++) {
-            printf("*  ");
-        }
-        printf("\n");
-    }
-    space = mspace;
-    for(int i = 0;i < mspace;i++) {
-        int j = 0;
-        for(j = 0;j < space;j++) {
-            printf("*  ");
-        }
-        int k = 0;
-        for(k = 0;k <2*i+1;k++) {
-            printf("   ");
-        }
-        for(int z = 0;z < m-j-k+2;z++) {
-            printf("*  ");
-        }
-        printf("\n");
-        space--;
-    }
+	int mstar = m/2;
+	int mspace = m-mstar;
+	int space = mspace;
+	for(int i = 0;i < mspace;i++) {
+		for(int k = 0;k < space;k++) {
+			printf("   ");
+		}
+		space--;
+		for(int j = 0;j < 2*i+1;j++) {
+			printf("*  ");
+		}
+		printf("\n");
+	}
+	for(int i = 0;i < mstar;i++) {
+		for(int j = 0;j < m+2;j++) {
+			printf("*  ");
+		}
+		printf("\n");
+	}
+	space = mspace;
+	for(int i = 0;i < mspace;i++) {
+		int j = 0;
+		for(j = 0;j < space;j++) {
+			printf("*  ");
+		}
+		int k = 0;
+		for(k = 0;k <2*i+1;k++) {
+			printf("   ");
+		}
+		for(int z = 0;z < m-j-k+2;z++) {
+			printf("*  ");
+		}
+		printf("\n");
+		space--;
+	}
 }
 int main(int argc,char** argv) {
-    mnstar(6, 7);
-    mstar(6);
-    rectstar(9);
-    doubstar(6r
-    (
-   rightsta 
-    rightstar5);
-    greatstar(5);
-    lessstar(5);
-    upstar(9);
-    printf("hello world!");
-}
+	mnstar(6, 7);
+	mstar(6);
+	rectstar(9);
+	doubstar(6r
+			(
+			 rightsta 
+			 rightstar5);
+			greatstar(5);
+			lessstar(5);
+			upstar(9);
+			printf("hello world!");
+			}
 
