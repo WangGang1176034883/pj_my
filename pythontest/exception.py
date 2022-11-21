0ee6bd8fd283bee668d0ab01e74d7a13b95b1171
wg <11760348833@qq.com>
2022-11-21 11:27:49 +0800

    v2022.11.21

--- pythontest/exception.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-try:
-    list=[10,8,9]
-    print(list[9])
-except Exception as e:
-    print("error!",e)
--- pythontest/req_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import requests
-import json
-res = requests.get("https://m.luffycity.com/api/v1/course/degree/3/?courseType=degree&id=3")
-dict = json.loads(res.text)
-for item in dict["data"].items():
-    k,v = item
-    print(k,v)
